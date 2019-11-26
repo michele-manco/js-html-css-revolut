c0a97f8819b255f8accdd9095ae0a8bf80d27330
Michele_Manco <motlamik@gmail.com>
2019-11-26 20:38:53 +0100

    3 ways done

---
+++ main.js
@@ -0,0 +1,24 @@
+// $('.menu-items').click(function(){
+//   $(this).children('.dropd-menu').toggle();
+// })
+
+
+
+// $('.menu-items').click(function() {
+//   if ($(this).children('.dropd-menu').is(':visible')) {
+//       $('.dropd-menu').slideUp();
+//   } else {
+//       $('.dropd-menu').slideUp();
+//       $(this).children('.dropd-menu').slideDown()
+//   }
+//
+// });
+$('.menu-items').click(function() {
+
+  $('.dropd-menu').slideUp();
+
+  if (!$(this).children('.dropd-menu').is(':visible')) {
+      $(this).children('.dropd-menu').slideDown()
+  }
+
+});
